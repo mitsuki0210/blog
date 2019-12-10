93c9421962238b1aa51c28ca860fa1bc8089b045
牟田光希 <mutamitsuki@mutamitsukinoMacBook-puro.local>
2019-12-10 16:40:55 +0900

    add new file

---
+++ docs/resources/js/store/index.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import auth from './auth'
+import error from './error' // ★ 追加
+import message from './message'
+
+Vue.use(Vuex)
+
+const store = new Vuex.Store({
+  modules: {
+    auth,
+    error, // ★ 追加
+    message
+  }
+})
+
+export default store
